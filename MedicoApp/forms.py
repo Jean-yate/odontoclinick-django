587fb113e86c4bd02d0d5d042525f73099b63a03
Jean-yate <jeanpierrey.g08@gmail.com>
2026-06-21 00:36:46 -0500

    Cambios Por testing, Validaciones de datos

--- MedicoApp/forms.py
+++ MedicoApp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import HistorialMedico
+from .models import HistorialMedico, Disponibilidad
 
 class RegistroConsultaForm(forms.ModelForm):
     class Meta:
@@ -11,3 +11,38 @@ class RegistroConsultaForm(forms.ModelForm):
             'id_tratamiento': forms.Select(attrs={'class': 'form-select'}),
             'costo_aplicado': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+class DisponibilidadForm(forms.ModelForm):
+    class Meta:
+        model = Disponibilidad
+        fields = ['dia_semana', 'hora_inicio', 'hora_fin', 'duracion_cita']
+
+    def clean_duracion_cita(self):
+        duracion = self.cleaned_data.get("duracion_cita")
+
+        if duracion is None:
+            return duracion
+
+        if duracion < 10:
+            raise forms.ValidationError("La duración mínima de una cita es 10 minutos.")
+
+        if duracion > 120:
+            raise forms.ValidationError("La duración máxima de una cita es 120 minutos.")
+
+        return duracion
+    
+    def clean(self):
+        cleaned_data = super().clean()
+    
+        inicio = cleaned_data.get("hora_inicio")
+        fin = cleaned_data.get("hora_fin")
+        duracion = cleaned_data.get("duracion_cita")
+    
+        if inicio and fin and inicio >= fin:
+            raise forms.ValidationError("La hora de inicio debe ser menor que la hora final.")
+    
+        if duracion:
+            if duracion < 10 or duracion > 120:
+                raise forms.ValidationError("La duración debe estar entre 10 y 120 minutos.")
+    
+        return cleaned_data
